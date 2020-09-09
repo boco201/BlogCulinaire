5be6d4db0af4690c0e3660f5fcdc166f63283f08
boco <elizaboco66@gmail.com>
2020-09-09 15:13:58 +0200

    m

--- app/Http/Controllers/Admin/CategoryController.php
+++ app/Http/Controllers/Admin/CategoryController.php
@@ -15,7 +15,7 @@ class CategoryController extends Controller
      */
     public function index()
     {
-        return view('admin.categories.index');
+        return view('admin.index');
         //
     }
 
@@ -27,7 +27,7 @@ class CategoryController extends Controller
     public function create()
     {
         
-        return view('admin.categories.create');
+        return view('admin.create');
     }
 
     /**
